c0c3021ab8e24bd51bbeba0ba1d07a99f6286948
Ziga <zurgaziga@gmail.com>
2026-05-28 10:03:10 +0200

    nekja

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -18,7 +18,7 @@ export default function Landing() {
 
           <button style={styles.register} onClick={() => navigate("/register")}>
             Register
-          </button>
+          </button>s
         </div>
       </div>
     </div>
