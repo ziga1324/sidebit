dd77fb5e4a292a4aa5f87bf4e8bdd5be74741f15
Ziga <zurgaziga@gmail.com>
2026-05-31 11:47:49 +0200

    fixi za MI2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import "./hom_e.css"
 import { Link, useNavigate } from "react-router-dom"
+import "./hom_e.css"
+
 
 const API_URL = import.meta.env.VITE_API_URL || ""
 
